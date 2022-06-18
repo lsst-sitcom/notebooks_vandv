bda3ff4f77857962e3ec525d29bfc490a8185cda
Bruno Quint <bquint@lsst.org>
2022-06-18 18:18:43 +0000

    Update m1m3.py with new plots

--- python/lsst/sitcom/vandv/m1m3.py
+++ python/lsst/sitcom/vandv/m1m3.py
@@ -24,7 +24,7 @@ def get_rms(s):
     cols = [c for c in s.index for fa in FATABLE if f"zForce{fa[1]}" in c]
     
     zforces = np.array(s["mtm1m3.forceActuatorData.zForce101"])
-    rms = np.sqrt((1 / zforces) * np.sum((lut - zforces) ** 2))
+    rms = np.sqrt((1 / zforces.size) * np.sum((lut - zforces) ** 2))
     return rms
 
 
@@ -152,6 +152,7 @@ def plot_m1m3_and_elevation(df, prefix=None):
         nrows=3
     )
     ax0b = ax0.twinx()
+    ax2b = ax2.twinx()
 
     # Resample for simplicity
     df = df.resample("0.5S").mean()
@@ -164,19 +165,21 @@ def plot_m1m3_and_elevation(df, prefix=None):
         zforce = df[f"mtm1m3.forceActuatorData.zForce{i}"].dropna()
         ax0.plot(zforce, "-", c=c, 
                  label=f"zForce{i} ({actuators_ids_table[i]})")
-        
-    # Plot elevation vs time
-    el = df["elevation"].dropna()    
-    ax0b.fill_between(el.index, 0, el, fc="black", alpha=0.1)
-    
+            
     # plot forces vs elevation
     for i, c in zip(actuators_ids_idx, colors):
         el = df["elevation"]
         zforce = df[f"mtm1m3.forceActuatorData.zForce{i}"]
         ax1.plot(el, zforce, "-", c=c, 
                  label=f"zForce{i} ({actuators_ids_table[i]})")        
-    # plot rms
+
+    # plot rms vs time
     ax2.plot(df["rms"], label="all actuators rms")
+    
+    # Plot elevation vs time
+    el = df["elevation"].dropna()    
+    ax0b.fill_between(el.index, 0, el, fc="black", alpha=0.1)
+    ax2b.fill_between(el.index, 0, el, fc="black", alpha=0.1)
     
     # Tweak axes
     ax0.set_xlabel("Time")
@@ -195,3 +198,69 @@ def plot_m1m3_and_elevation(df, prefix=None):
     ax2.legend()
     
     plt.show()
+
+    
+async def plotM1M3Forces(mtm1m3):
+    from lsst.ts.cRIOpy import M1M3FATable
+
+    fel = await mtm1m3.evt_appliedElevationForces.aget(timeout=10.)
+    faz = await mtm1m3.evt_appliedAzimuthForces.aget(timeout=10.)
+    fth = await mtm1m3.evt_appliedThermalForces.aget(timeout=10.)
+    fba = await mtm1m3.evt_appliedBalanceForces.aget(timeout=10.)
+    fac = await mtm1m3.evt_appliedAccelerationForces.aget(timeout=10.)
+    fve = await mtm1m3.evt_appliedVelocityForces.aget(timeout=10.)
+    fst = await mtm1m3.evt_appliedStaticForces.aget(timeout=10.)
+    fab = await mtm1m3.evt_appliedAberrationForces.aget(timeout=10.)
+    fof = await mtm1m3.evt_appliedOffsetForces.aget(timeout=10.)
+    fao = await mtm1m3.evt_appliedActiveOpticForces.aget(timeout=10.)
+    fapp = await mtm1m3.evt_appliedForces.aget(timeout=10.)
+
+    ftel = await mtm1m3.tel_forceActuatorData.next(flush=True, timeout=10.)
+    
+    # Get the position of the actuators
+    fat = np.array(M1M3FATable.FATABLE)
+    xact = np.float64(fat[:, M1M3FATable.FATABLE_XPOSITION])
+    yact = np.float64(fat[:, M1M3FATable.FATABLE_YPOSITION])
+        
+    # Create the plot
+    fig, ax = plt.subplots(3,1, figsize=(15,8))
+    print(fel.xForces)
+    ax[0].plot(fel.xForces, '-o', label='elevation');
+    ax[0].plot(fba.xForces, label='FB')
+    ax[0].plot(fst.xForces, label='static')
+    ax[0].plot(ftel.xForce, '-v', label='measured')
+    ax[0].legend()
+    ax[0].set_title('XForces')
+    
+    ax[1].plot(fel.yForces, '-o', label='elevation');
+    ax[1].plot(fba.yForces, label='FB')
+    ax[1].plot(fst.yForces, label='static')
+    ax[1].plot(ftel.yForce, '-v', label='measured')
+    ax[1].legend()
+    ax[1].set_title('YForces')
+    ax[2].plot(fel.zForces, '-o', label='elevation');
+    ax[2].plot(fba.zForces, label='FB')
+    ax[2].plot(fst.zForces, label='static')
+    ax[2].plot(fao.zForces, label='AOS')
+    ax[2].plot(ftel.zForce, '-v', label='measured')
+    ax[2].set_title('ZForces')
+    ax[2].legend()
+    
+    fig2, ax=plt.subplots( 1,3, figsize = [15,4])
+    aa = np.array(fao.zForces)
+    img = ax[0].scatter(xact, yact, c=aa)
+    ax[0].axis('equal')
+    ax[0].set_title('AOS forces')
+    fig.colorbar(img, ax=ax[0])
+
+    aa = np.array(fel.zForces)
+    img = ax[1].scatter(xact, yact, c=aa)
+    ax[1].axis('equal')
+    ax[1].set_title('elevation forces')
+    fig.colorbar(img, ax=ax[1])
+    
+    aa = np.array(fst.zForces)
+    img = ax[2].scatter(xact, yact, c=aa)
+    ax[2].axis('equal')
+    ax[2].set_title('static forces')
+    fig.colorbar(img, ax=ax[2])
