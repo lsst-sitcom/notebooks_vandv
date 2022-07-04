efa309cb4a7b91277407a08bd48a20ba108cd953
Bruno Quint <bquint@lsst.org>
2022-07-04 15:57:46 +0000

    [WIP] Update plots for hexapod in LVV-T2213

--- python/lsst/sitcom/vandv/__init__.py
+++ python/lsst/sitcom/vandv/__init__.py
@@ -2,4 +2,5 @@ from . import efd
 from . import hexapod
 from . import m1m3
 from . import m2
+from . import mount
 from .info import *
--- python/lsst/sitcom/vandv/mount.py
+++ python/lsst/sitcom/vandv/mount.py
@@ -1,35 +1,41 @@
 import asyncio
+import time
 
+       
+async def moveMountInElevationSteps(mount, target_el, azimuth=0, step_size=0.25, time_sleep=1):
+    """Move the mount from the current elevation angle to the target elevation angle 
+    in steps to avoid any issues whe M1M3 and/or M2 are running with the LUT using the 
+    Mount instead of the inclinometer.
+    
+    This function will actually calculate the number of steps using the ceiling
+    in order to make sure that we move carefully. 
+    
+    Parameters
+    ----------
+    mtmount : Remote 
+        Mount CSC remote. 
+    target_el : float
+        Target elevation angle in degrees
+    azimuth : float
+        Azimuth angle in degres (default)
+    step_size : float
+        Step elevation size in degrees (default: 0.25)
+    time_sleep : float
+        Sleep time between movements (default: 1)
+        
+    Returns
+    -------
+    azimuth : float
+        Current azimuth
+    elevation : float
+        Current elevation
+    """
+    current_el = mtmount.tel_elevation.get().actualPosition
+    n_steps = int(np.ceil(np.abs(current_el - target_el) / step_size))
 
-async def moveMountConstantV(mount, startAngle, stopAngle):
-    """Change the elevation angle step by step"""
-    # change the elevation angle step by step
-
-    freq = 1  # Hz
-    vAngle = 2  # 1 deg change per minute
-    holdMinutes = 0.1  # how long to hold at integeter values of the elevation angle
-    angleStepSize = 1  # each time we change by 1 deg, before we hold in place
-
-    rampMinutes = angleStepSize / vAngle
-    print(
-        "This will run for %.0f minutes"
-        % ((startAngle - stopAngle) * (rampMinutes + holdMinutes))
-    )
-    start_time = Time(datetime.now())
-    startTime = time.time()
-    end_time = start_time + timedelta(minutes=80)
-    demandAngle = startAngle
-    while demandAngle > stopAngle - 0.01:
-        await asyncio.sleep(1.0 / freq)
-
-        timeNow = time.time()
-        minutesEllapsed = (timeNow - startTime) / 60
-        cyclePassed = np.floor(minutesEllapsed / (rampMinutes + holdMinutes))
-        minutesIntoThisCycle = min(
-            rampMinutes, minutesEllapsed - cyclePassed * (rampMinutes + holdMinutes)
-        )
-        demandAngle = (
-            startAngle - cyclePassed * angleStepSize - minutesIntoThisCycle * vAngle
-        )
-        # print(demandAngle, cyclePassed, minutesIntoThisCycle)
-        await mount.cmd_moveToTarget.set_start(azimuth=0, elevation=demandAngle)
+    for el in np.linspace(current_el, target_el, n_steps):
+        print(f"Moving elevation to {el:.2f} deg")
+        await mtmount.cmd_moveToTarget.set_start(azimuth=azimuth, elevation=el)
+        time.sleep(time_sleep)
+        
+    return azimuth, el
