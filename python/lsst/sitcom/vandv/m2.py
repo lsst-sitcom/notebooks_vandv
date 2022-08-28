1b64360298aaa5e86bea6b1dd67b987a46f7e726
ISotuela <isotuela@lsst.org>
2022-08-28 02:09:28 +0000

    Address PR comments

--- python/lsst/sitcom/vandv/m2.py
+++ python/lsst/sitcom/vandv/m2.py
@@ -397,7 +397,7 @@ def snapshot_zforces_overview(
     ax,
     series,
     prefix="measured",
-    title="", 
+    title=None, 
     show_ids=True,
     show_mirrors=True,
     lut_path=None,
@@ -477,9 +477,9 @@ def snapshot_zforces_overview(
             arrowprops=dict(arrowstyle="->"), ha="center", va="center",) 
 
     ax.axis("equal")
+    title = title if title else f"{prefix} axial forces"
     ax.set_title(title)
 
-    #ax.set_title(f"{prefix} axial forces")
     ax.set_axis_off()
 
     divider = make_axes_locatable(ax)
