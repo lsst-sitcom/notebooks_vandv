2e68096a7445c910536ba6e3888224263d62a315
psferguson <petersferguson@gmail.com>
2023-11-21 12:37:16 -0800

    squash

--- python/lsst/sitcom/vandv/m1m3/sitcomtn81/sitcomtn_81_identify_oscillations.py
+++ python/lsst/sitcom/vandv/m1m3/sitcomtn81/sitcomtn_81_identify_oscillations.py
@@ -13,6 +13,7 @@ import os
 import pandas as pd
 import numpy as np
 from astropy.time import Time
+import math
 
 from lsst_efd_client import EfdClient
 from lsst.summit.utils.tmaUtils import TMAEventMaker, TMAState
@@ -23,6 +24,26 @@ from scipy.signal import find_peaks
 from scipy.interpolate import interp1d
 from tqdm import tqdm
 
+def clipDataToEvent(df, event, padding):
+    """Clip a padded dataframe to an event.
+
+    Parameters
+    ----------
+    df : `pandas.DataFrame`
+        The dataframe to clip.
+    event : `lsst.summit.utils.efdUtils.TmaEvent`
+        The event to clip to.
+
+    Returns
+    -------
+    clipped : `pandas.DataFrame`
+        The clipped dataframe.
+    """
+    mask = (df["private_sndStamp"] >= event.begin.value - padding) & (
+        df["private_sndStamp"] <= event.end.value + padding
+    )
+    clipped_df = df.loc[mask].copy()
+    return clipped_df
 
 class IdentifyOscillationEvents:
     """
@@ -72,17 +93,19 @@ class IdentifyOscillationEvents:
         identifies oscillation events.
     """
 
-    def __init__(self):
+    def __init__(self, efd_instance="idf_efd"):
         self.force = "two"
         self.rolling_std_window = 100  # 100 is ~ 2 second window
         self.association_window_1 = (
-            2  # window in seconds to combine peaks in same actuator
+            6  # window in seconds to combine peaks in same actuator
         )
         self.association_window_2 = (
-            4  # window in seconds to combine peaks accross actuators
+            7  # window in seconds to combine peaks accross actuators
         )
         self.slew_speed_min = 0.01  # used for identifiying when we are slewing
-        self.peak_height = 100
+        self.peak_height = 50
+        self.efd_instance = efd_instance
+        self.client = EfdClient(self.efd_instance)
 
     def add_timestamp(self, data):
         """
@@ -151,7 +174,7 @@ class IdentifyOscillationEvents:
                 {
                     "times": super_times,
                     "heights": super_heights,
-                    "rmean": super_rmean,
+                    "rmean_diff": super_rmean,
                     "counts": super_counts,
                     "actuators": super_actuators,
                 }
@@ -167,12 +190,12 @@ class IdentifyOscillationEvents:
         super_actuators = super_actuators[super_inds]
         super_counts = super_counts[super_inds]
         super_rmean = super_rmean[super_inds]
-        # import pdb;pdb.set_trace()
+
         return pd.DataFrame(
             {
                 "times": super_times,
                 "heights": super_heights,
-                "rmean": super_rmean,
+                "rmean_diff": super_rmean,
                 "counts": super_counts,
                 "actuators": super_actuators,
             }
@@ -192,13 +215,12 @@ class IdentifyOscillationEvents:
         list
             A list of slewing events.
         """
-        client = EfdClient("idf_efd")
-        event_maker = TMAEventMaker(client)
-        events = event_maker.getEvents(int(day_obs))
+        eventMaker = TMAEventMaker(self.client)
+        events = eventMaker.getEvents(int(day_obs),)
         slews = [e for e in events if e.type == TMAState.SLEWING]
-        return slews, client
+        return slews
 
-    async def get_data(self, event, client):
+    async def get_data(self, begin, end, client):
         """
         Asynchronously extracts and processes MTMount data from the EFD for a
         given event.
@@ -213,37 +235,16 @@ class IdentifyOscillationEvents:
 
         # Get EFD client options are usdf_efd or summit_efd
 
-        self.query_dict = {}
-        self.query_dict["day_obs"] = event.dayObs
-        self.query_dict["seq_num"] = event.seqNum
-        self.query_dict["el"] = getEfdData(
-            client,
-            "lsst.sal.MTMount.elevation",
-            event=event,
-            prePadding=5,
-            postPadding=5,
-        )
+        # Get EFD client options are usdf_efd or summit_efd
 
-        if "private_sndStamp" not in self.query_dict["el"].keys():
-            print("no el data")
-            self.query_dict = None
-            return
-        self.query_dict["el"] = self.add_timestamp(self.query_dict["el"])
-
-        self.query_dict["az"] = getEfdData(
-            client, "lsst.sal.MTMount.azimuth", event=event, prePadding=5, postPadding=5
-        )
-
-        if "private_sndStamp" not in self.query_dict["az"].keys():
-            print("no az data")
-            self.query_dict = None
-            return
-        self.query_dict["az"] = self.add_timestamp(self.query_dict["az"])
-
-        self.query_dict["hpmf"] = getEfdData(
+        query_dict = {}
+        query_dict["day_obs"] = self.day_obs
+        
+        query_dict["hpmf"] = getEfdData(
             client,
             "lsst.sal.MTM1M3.hardpointActuatorData",
-            event=event,
+            begin=begin,
+            end=end,
             prePadding=5,
             postPadding=5,
             columns=[
@@ -255,20 +256,72 @@ class IdentifyOscillationEvents:
                 "measuredForce4",
                 "measuredForce5",
             ],
+            warn=False,
         )
-        if "private_sndStamp" not in self.query_dict["hpmf"].keys():
+        if "private_sndStamp" not in query_dict["hpmf"].keys():
             print("no hpmf data")
-            self.query_dict = None
+            query_dict = None
             return
-        self.query_dict["hpmf"] = self.add_timestamp(self.query_dict["hpmf"])
+        query_dict["hpmf"] = self.add_timestamp(query_dict["hpmf"])
 
-    def identify(self, query_dict):
+        query_dict["el"] = getEfdData(
+            client,
+            "lsst.sal.MTMount.elevation",
+            columns=["private_sndStamp", "actualPosition", "actualVelocity"],
+            begin=begin,
+            end=end,
+            prePadding=10,
+            postPadding=10,
+            warn=False,
+        )
+
+        if "private_sndStamp" not in query_dict["el"].keys():
+            print("no el data")
+            query_dict = None
+            return
+        query_dict["el"] = self.add_timestamp(query_dict["el"])
+
+        query_dict["az"] = getEfdData(
+            client,
+            "lsst.sal.MTMount.azimuth",
+            columns=["private_sndStamp", "actualPosition", "actualVelocity"],
+            begin=begin,
+            end=end,
+            prePadding=5,
+            postPadding=5,
+            warn=False,
+        )
+
+        if "private_sndStamp" not in query_dict["az"].keys():
+            print("no az data")
+            query_dict = None
+            return
+        query_dict["az"] = self.add_timestamp(query_dict["az"])
+        return query_dict
+
+    def get_single_slew_data_dict(self, event):
+        slew_dict = {}
+        for key in self.query_dict.keys():
+            if isinstance(self.query_dict[key], pd.DataFrame):
+                slew_dict[key] = clipDataToEvent(
+                    self.query_dict[key], event, padding=10
+                )
+            else:
+                slew_dict[key] = self.query_dict[key]
+        slew_dict["seq_num"] = event.seqNum
+        for key in self.query_dict.keys():
+            if key != "day_obs":
+                if len(slew_dict[key]) < 1:
+                    return None
+        return slew_dict
+
+    def identify(self, data_dict):
         """
         Identifies oscillation events based on the processed query data.
 
         Parameters
         ----------
-        query_dict : dict
+        data_dict : dict
             The dictionary containing query data for oscillation event
             identification.
 
@@ -278,29 +331,32 @@ class IdentifyOscillationEvents:
             A DataFrame containing identified oscillation events,
             or None if no events are identified.
         """
-        if query_dict is None:
+        if data_dict is None:
             return None
 
         peak_dict = {}
-        peak_frame = pd.DataFrame({"times": [], "heights": [], "actuators": []})
+        peak_frame = pd.DataFrame({"times": [], 
+                                   "heights": [], 
+                                   "actuators": [],
+                                   "rmean_diff":[]})
         for i in range(6):
             # this loop identifies rolling std peaks in the measured force
             rolling_std_val = (
-                self.query_dict["hpmf"][f"measuredForce{i}"]
+                data_dict["hpmf"][f"measuredForce{i}"]
                 .rolling(self.rolling_std_window)
                 .std()
             )  # 100 is ~ 2 second window
             rolling_mean_val = (
-                self.query_dict["hpmf"][f"measuredForce{i}"]
+                data_dict["hpmf"][f"measuredForce{i}"]
                 .rolling(self.rolling_std_window)
                 .mean()
             )
             peak_indicies = find_peaks(rolling_std_val, height=self.peak_height)[0]
 
             # keep time and height of peaks
-            peak_dict[f"hp_{i}_peak_times"] = self.query_dict["hpmf"][
-                "snd_timestamp_utc"
-            ][peak_indicies].values
+            peak_dict[f"hp_{i}_peak_times"] = data_dict["hpmf"]["snd_timestamp_utc"][
+                peak_indicies
+            ].values
             peak_dict[f"hp_{i}_peak_heights"] = rolling_std_val[peak_indicies].values
             start_ind = [np.max([i - 500, 0]) for i in peak_indicies]
             stop_ind = [
@@ -330,78 +386,81 @@ class IdentifyOscillationEvents:
                 super_times.append(peak_dict[f"hp_{i}_peak_times"][max_index])
                 super_heights.append(peak_dict[f"hp_{i}_peak_heights"][max_index])
                 super_rmean.append(peak_dict[f"hp_{i}_peak_rmean_diff"][max_index])
-            peak_dict[f"hp_{i}_peak_times"] = np.unique(super_times)
-            peak_dict[f"hp_{i}_peak_heights"] = np.unique(super_heights)
-            peak_dict[f"hp_{i}_peak_rmean_diff"] = np.unique(super_rmean)
+            if len(super_times) > 0:
 
-            peak_frame = pd.concat(
-                [
-                    peak_frame,
-                    pd.DataFrame(
-                        {
-                            "times": peak_dict[f"hp_{i}_peak_times"],
-                            "heights": peak_dict[f"hp_{i}_peak_heights"],
-                            "rmean_diff": peak_dict[f"hp_{i}_peak_rmean_diff"],
-                            "actuators": i,
-                        }
-                    ),
-                ]
-            )
-            peak_frame = peak_frame.sort_values("times")
+                peak_dict[f"hp_{i}_peak_times"], time_index = np.unique(np.concatenate(super_times), return_index=True)
+                peak_dict[f"hp_{i}_peak_heights"] = np.concatenate(super_heights)[time_index]
+                peak_dict[f"hp_{i}_peak_rmean_diff"] = np.concatenate(super_rmean)[time_index]
+                try:
+                    new_frame=pd.DataFrame(
+                                {
+                                    "times": peak_dict[f"hp_{i}_peak_times"],
+                                    "heights": peak_dict[f"hp_{i}_peak_heights"],
+                                    "rmean_diff": peak_dict[f"hp_{i}_peak_rmean_diff"],
+                                    "actuators": i,
+                                }
+                            )
+                except:
+                    import pdb; pdb.set_trace()
+                peak_frame = pd.concat(
+                    [
+                        peak_frame,
+                        new_frame,
+                    ], 
+                )
+        peak_frame = peak_frame.sort_values("times")
 
-            # next we want to combine peaks across actuators
-            overall_frame = self.combine_peaks_accross_actuators(
-                peak_frame, window=self.association_window_2
-            )
+        # next we want to combine peaks across actuators
+        overall_frame = self.combine_peaks_accross_actuators(
+            peak_frame, window=self.association_window_2
+        )
 
-            # identify when we are slewing
-            overall_frame["slew_state"] = False
-            slew_speed_el = interp1d(
-                self.query_dict["el"]["snd_timestamp_utc"],
-                abs(self.query_dict["el"]["actualVelocity"].rolling(10).mean()),
-                bounds_error=False,
-            )
-            slew_speed_az = interp1d(
-                self.query_dict["az"]["snd_timestamp_utc"],
-                abs(self.query_dict["az"]["actualVelocity"].rolling(10).mean()),
-                bounds_error=False,
-            )
+        # identify when we are slewing
+        overall_frame["slew_state"] = False
+        slew_speed_el = interp1d(
+            data_dict["el"]["snd_timestamp_utc"],
+            abs(data_dict["el"]["actualVelocity"].rolling(10).mean()),
+            bounds_error=False,
+        )
+        slew_speed_az = interp1d(
+            data_dict["az"]["snd_timestamp_utc"],
+            abs(data_dict["az"]["actualVelocity"].rolling(10).mean()),
+            bounds_error=False,
+        )
 
-            slew_velocity_el = interp1d(
-                self.query_dict["el"]["snd_timestamp_utc"],
-                (self.query_dict["el"]["actualVelocity"].rolling(10).mean()),
-                bounds_error=False,
-            )
-            slew_velocity_az = interp1d(
-                self.query_dict["az"]["snd_timestamp_utc"],
-                (self.query_dict["az"]["actualVelocity"].rolling(10).mean()),
-                bounds_error=False,
-            )
+        slew_velocity_el = interp1d(
+            data_dict["el"]["snd_timestamp_utc"],
+            (data_dict["el"]["actualVelocity"].rolling(10).mean()),
+            bounds_error=False,
+        )
+        slew_velocity_az = interp1d(
+            data_dict["az"]["snd_timestamp_utc"],
+            (data_dict["az"]["actualVelocity"].rolling(10).mean()),
+            bounds_error=False,
+        )
 
-            slew_position = interp1d(
-                self.query_dict["el"]["snd_timestamp_utc"],
-                (self.query_dict["el"]["actualPosition"].rolling(10).mean()),
-                bounds_error=False,
-            )
+        slew_position = interp1d(
+            data_dict["el"]["snd_timestamp_utc"],
+            (data_dict["el"]["actualPosition"].rolling(10).mean()),
+            bounds_error=False,
+        )
 
-            sel = slew_speed_el(overall_frame["times"]) > self.slew_speed_min
-            sel |= slew_speed_az(overall_frame["times"]) > self.slew_speed_min
-            overall_frame.loc[sel, "slew_state"] = True
-            overall_frame["elevation_velocity"] = slew_velocity_el(
-                overall_frame["times"]
-            )
-            overall_frame["azimuth_velocity"] = slew_velocity_az(overall_frame["times"])
-            overall_frame["elevation_position"] = slew_position(overall_frame["times"])
-            overall_frame = overall_frame.loc[overall_frame["slew_state"] == True, :]
+        sel = slew_speed_el(overall_frame["times"]) > self.slew_speed_min
+        sel |= slew_speed_az(overall_frame["times"]) > self.slew_speed_min
+        overall_frame.loc[sel, "slew_state"] = True
+        overall_frame["elevation_velocity"] = slew_velocity_el(overall_frame["times"])
+        overall_frame["azimuth_velocity"] = slew_velocity_az(overall_frame["times"])
+        overall_frame["elevation_position"] = slew_position(overall_frame["times"])
+        overall_frame = overall_frame.loc[overall_frame["slew_state"] == True, :]
 
-            if len(overall_frame) > 0:
-                overall_frame["seq_num"] = query_dict["seq_num"]
-                overall_frame["day_obs"] = query_dict["day_obs"]
-                return overall_frame
-            else:
-                return None
+        if len(overall_frame) > 0:
+            overall_frame["seq_num"] = data_dict["seq_num"]
+            overall_frame["day_obs"] = self.day_obs
+            return overall_frame
+        else:
+            return None
 
-    async def run(self, dayObs):
+    async def run(self, day_obs):
         """
         Asynchronously processes data for a given observation day and identifies
         potential oscillation events.
@@ -417,22 +476,71 @@ class IdentifyOscillationEvents:
             A DataFrame containing all identified events for the given day, or None if no events
             are identified.
         """
+        
+        self.day_obs = day_obs
+        self.slews = await self.get_slews(day_obs)
+        if len(self.slews) == 0:
+            print("no data")
+            return None
+        else:
+            print(f"{day_obs} {len(self.slews)} slews identified")
+        self.slews = self.slews
 
-        self.slews, client = await self.get_slews(dayObs)
         # make query
+        if len(self.slews) < 2000:
+            self.query_dict = await self.get_data(self.slews[0].begin, self.slews[-1].end, self.client)
+        else:
+            chunk_size= 1
+            # Calculate the number of chunks needed
+            nchunks = math.ceil(len(self.slews) / chunk_size)
+            
+            # Initialize an empty list to store results from each chunk
+            chunked_results = []
 
+            # Iterate over the chunks
+            print("chunking")
+            for i in range(nchunks):
+                print(f"chunk {i}")
+                # Calculate start and end indices for each chunk
+                start_index = i * chunk_size
+                end_index = min((i + 1) * chunk_size, len(self.slews))
+                print(f"start: {self.slews[start_index].begin}")
+                print(f"end: {self.slews[end_index - 1].end}")
+                # Get the data for the current chunk and append it to the list
+                chunk_data = await self.get_data(self.slews[start_index].begin, self.slews[end_index - 1].end, self.client)
+                chunked_results.append(chunk_data)
+            
+            keys = chunked_results[0].keys()
+            self.query_dict = {}
+            # Combine the data for each key
+            for key in keys:
+                if key != "day_obs":
+                    combined_data = pd.concat([result[key] for result in query_results])
+                    self.query_dict[key] = combined_data
+                else:
+                    # Assuming 'day_obs' is a list and not a DataFrame
+                    self.query_dict["day_obs"] = self.day_obs
+
+                 
         event_list = []
+
         for slew in tqdm(self.slews):
-            await self.get_data(self.slews[0], client)
-            # np.warnings.filterwarnings('error', category=np.VisibleDeprecationWarning)
-            result = self.identify(self.query_dict)
+            slew_dict = self.get_single_slew_data_dict(slew)
+            if slew_dict is None:
+                continue
+            result = self.identify(slew_dict)
             if result is not None:
                 event_list.append(result)
         if len(event_list) > 0:
             events_frame = pd.concat(event_list)
             return events_frame
         else:
-            return None
+            empty_frame=pd.DataFrame()
+            for key in ['times', 'heights', 'rmean_diff', 'counts', 'actuators', 'slew_state',
+       'elevation_velocity', 'azimuth_velocity', 'elevation_position',
+       'seq_num', 'day_obs']:
+                empty_frame[key]=[]
+            return empty_frame
 
 
 if __name__ == "__main__":
